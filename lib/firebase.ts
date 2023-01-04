4e0929d16e30ea25b6f72ca6bb73f16902d9be28
Chris Hendrix <chendrix1123@gmail.com>
2023-01-03 20:50:16 -0500

    setup firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDGKFefmMIJvb4cscMpTm_s_digAwkMiQQ',
+  authDomain: 'nextfire-da517.firebaseapp.com',
+  projectId: 'nextfire-da517',
+  storageBucket: 'nextfire-da517.appspot.com',
+  messagingSenderId: '901276707375',
+  appId: '1:901276707375:web:dae3e38da90c75ac8d74da'
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+export const auth = getAuth(firebaseApp)
+export const firestore = getFirestore(firebaseApp)
+export const storage = getStorage(firebaseApp)
